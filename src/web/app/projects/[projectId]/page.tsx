9c6062e96a9f8e5556f8c0a0a9c6b6df9b340d25
ehsangholamibbq-cell <ehsan.gholami.bbq@gmail.com>
2026-09-17 19:32:26 +0330

    Accept canonical stored project identifiers in Web routes

--- src/web/app/projects/[projectId]/page.tsx
+++ src/web/app/projects/[projectId]/page.tsx
@@ -5,7 +5,7 @@ interface ProjectPageProps {
   readonly params: Promise<{ projectId: string }>;
 }
 
-const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu;
+const uuidPattern = /^[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12}$/iu;
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
   const { projectId } = await params;
